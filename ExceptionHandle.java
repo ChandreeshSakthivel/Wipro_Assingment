c48297a63e54aa0edd51b9a2ba65f37713b9c9f9
ChandreeshSakthivel <chandreeshsk@gmail.com>
2025-02-05 09:00:14 +0530

    Assignment-1

---
+++ ExceptionHandle.java
@@ -0,0 +1,21 @@
+public class Exceptionhandle {
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("Caught NullPointerException: " + e.getMessage());
+        } finally {
+            System.out.println("Finally block executed after NullPointerException");
+        }
+        
+        try {
+            int[] arr = new int[5];
+            System.out.println(arr[10]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Caught ArrayIndexOutOfBoundsException: " + e.getMessage());
+        } finally {
+            System.out.println("Finally block executed after ArrayIndexOutOfBoundsException");
+        }
+    }
+}
